519943bac72c5aaa51a9f862226868ed5e484b60
Simon Victory <email@simonvictory.com>
2016-07-30 19:47:12 +1000

    lots of shit happened

--- client/app/app.js
+++ client/app/app.js
@@ -2,7 +2,7 @@
 
 angular.module('govhackApp', ['govhackApp.auth', 'govhackApp.admin', 'govhackApp.constants',
     'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
-    'validation.match'
+    'validation.match', 'chart.js', //'uiGmapgoogle-maps'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,6 +5,7 @@
   class MainController {
 
     constructor($http, $scope, socket) {
+      this.$scope = $scope;
       this.$http = $http;
       this.socket = socket;
       this.awesomeThings = [];
@@ -12,6 +13,28 @@
       $scope.$on('$destroy', function() {
         socket.unsyncUpdates('thing');
       });
+
+      $scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
+
+      $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+      $scope.data = [
+        [65, -59, 80, 81, -56, 55, -40],
+        [28, 48, -40, 19, 86, 27, 90]
+      ];
+      $scope.datasetOverride = [
+        {
+          label: "Bar chart",
+          borderWidth: 1,
+          type: 'bar'
+        },
+        {
+          label: "Line chart",
+          borderWidth: 3,
+          hoverBackgroundColor: "rgba(255,99,132,0.4)",
+          hoverBorderColor: "rgba(255,99,132,1)",
+          type: 'line'
+        }
+      ];
     }
 
     $onInit() {
@@ -34,6 +57,8 @@
     deleteThing(thing) {
       this.$http.delete('/api/things/' + thing._id);
     }
+
+
   }
 
   angular.module('govhackApp')
